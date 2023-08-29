0a895e46eadcaa62d6fdc243d2e4b37c0031f2b3
ashish112 <ashishraj143@gmail.com>
2023-08-29 15:19:38 +0530

    second

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "welcome to DIET collage......." );
+        System.out.println( "welcome to DIET workshop......." );
     }
 }
