267426c7996a8aac5a3d263046ba39e3a715b467
ashish112 <ashishraj143@gmail.com>
2023-08-29 14:30:05 +0530

    first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * welcome to DIET
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to DIET collage......." );
+    }
+}
